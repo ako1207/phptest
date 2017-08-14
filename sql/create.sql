aa27e68b67a700504b7d71555f2ba1027605f312
TC-17-026 <tc17026@TC-17-026noMacBook-Pro.local>
2017-08-14 15:29:32 +0900

    sql

---
+++ sql/create.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE ps3gbook DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
---
+++ sql/user.sql
@@ -0,0 +1,3 @@
+CREATE TABLE user(id int primary key,username varchar(255),password varchar(255));
+
+INSERT INTO user(id,username,password) value(17038,'高橋憧',1207);

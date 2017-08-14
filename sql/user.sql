34ce5e2de86f259f31851a92cdf2f73a57e2a0f8
TC-17-026 <tc17026@TC-17-026noMacBook-Pro.local>
2017-08-14 15:45:39 +0900

    sal part2

--- sql/user.sql
+++ sql/user.sql
@@ -1,3 +1,4 @@
+use ps3gbook
 CREATE TABLE user(id int primary key,username varchar(255),password varchar(255));
 
-INSERT INTO user(id,username,password) value(17038,'高橋憧',1207);
+INSERT INTO user(id,username,password) value(11111,'test',1207);
